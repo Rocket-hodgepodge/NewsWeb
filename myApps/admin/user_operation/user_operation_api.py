f193923add489629e078463d0d12e4ca4f2ce07c
Administrator <1021766585@qq.com>
2018-07-04 22:38:15 +0800

    models修改,和redis的配置

--- myApps/admin/user_operation/user_operation_api.py
+++ myApps/admin/user_operation/user_operation_api.py
@@ -3,10 +3,22 @@
 AUTH:
 DATA:
 """
+from io import BytesIO
 
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from myApps.untils.verify_code import VerifyCode
 
 
 def hello_user_operation(request):
     return HttpResponse('Hello User Operation')
+
+
+def get_verify_code(request):
+    vcode = VerifyCode()
+    image = vcode.verify_image
+    request.session['verify_code'] = vcode.verify_code
+    f = BytesIO()
+    image.save(f, 'jpeg')
+    resp = HttpResponse(f.getvalue(), content_type="image/jpeg")
+    return resp
--- myApps/admin/user_operation/user_operation_api_urls.py
+++ myApps/admin/user_operation/user_operation_api_urls.py
@@ -4,4 +4,6 @@ from myApps.admin.user_operation import user_operation_api
 
 urlpatterns = [
     path('', user_operation_api.hello_user_operation),
+    path('getVerifyCode/', user_operation_api.get_verify_code),
+
 ]
