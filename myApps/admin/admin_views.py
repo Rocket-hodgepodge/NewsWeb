0ae1da9031a0e697f85758c0d94313f7effa07bf
Administrator <1021766585@qq.com>
2018-07-05 09:56:15 +0800

    添加参数错误处理异常

--- myApps/admin/admin_views.py
+++ myApps/admin/admin_views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from myApps.untils.wrapper_set import is_login
+
 """
 后台视图页面
 该视图所有页面均需要登录才能访问,后续添加判断的装饰器即可
@@ -8,21 +10,26 @@ DATE: 2018年7月5日 02:12:57
 """
 
 
+@is_login
 def index(request):
     return render(request, 'admin/index.html')
 
 
+@is_login
 def news_manage(request):
     return render(request, 'admin/news_manage.html')
 
 
+@is_login
 def review_manage(request):
     return render(request, 'admin/review_manage.html')
 
 
+@is_login
 def role_manage(request):
     return render(request, 'admin/role_manage.html')
 
 
+@is_login
 def per_manage(request):
     return render(request, 'admin/per_manage.html')
