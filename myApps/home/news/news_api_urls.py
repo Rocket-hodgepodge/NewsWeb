cfbbe8d42cce0de7b9419b0848aa1beb72e08415
thisMoments <ofthemoment@163.com>
2018-07-06 21:10:19 +0800

    first

--- myApps/home/news/news_api_urls.py
+++ myApps/home/news/news_api_urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from myApps.home.news import news_liked_api, news_operation_api
 
 urlpatterns = [
-    path('liked/', news_liked_api.hello_news_liked),
     path('', news_operation_api.hello_news_operation),
+    path('liked/', news_liked_api.hello_news_liked),
+    path('news_search/', news_operation_api.news_title_search),  # 测试用
+    path('news_type/', news_operation_api.news_type_search),  # 测试用
+    path('newsSearch/', news_operation_api.news_search),
 ]
